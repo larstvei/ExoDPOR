511be4cd63c76c642182caa2dbe73e034710c91e
Lars Tveito <larstvei@ifi.uio.no>
2020-02-04 17:50:59 +0100

    Use backsets
    Implements a possible way of updating backsets, but it is not entirely correct.
    The "missing piece" is a correct implementation of /initials/ as they're called
    in the literature.

--- src/exogenous/dpor.clj
+++ src/exogenous/dpor.clj
@@ -1,5 +1,5 @@
 (ns exogenous.dpor
-  (:require [clojure.set :refer [union difference]]))
+  (:require [clojure.set :refer [union difference intersection]]))
 
 (defn new-node [ev enabled]
   {:backset #{ev} :enabled enabled :blocked #{} :sleep #{ev}})
@@ -13,6 +13,25 @@
             next-sleep (filter independent? sleep)]
         {:sleep (into #{} next-sleep)})))
 
+(defn not-dep [t ev e interference]
+  (let [independent? (comp not interference (partial vector e))]
+    (conj (vec (take-while independent? t)) ev)))
+
+(defn update-backsets [s trace ev interference]
+  (-> (fn [s i]
+        (let [e (trace i)
+              pre (subvec trace 0 i)
+              v (not-dep (subvec trace (inc i)) ev e interference)
+              n (s pre)
+              ;; This is not correct
+              initial (into #{} (filter #((:enabled n) %) v))]
+          (if (and (not-empty initial)
+                   (interference [e ev])
+                   (empty? (intersection initial (:backset n))))
+            (update-in s [pre :backset] conj (first initial))
+            s)))
+      (reduce s (range (count trace)))))
+
 (defn add-trace [search-state trace enabled interference]
   (-> (fn [s i]
         (let [t1 (subvec trace 0 i)
@@ -21,7 +40,8 @@
               node (update-node (s t1) ev (enabled i))
               next-node (next-sleep-set (s t2) ev (:sleep node) interference)]
           (-> (assoc s t1 node)
-              (assoc t2 next-node))))
+              (assoc t2 next-node)
+              (update-backsets t1 ev interference))))
       (reduce search-state (range (count trace)))
       (update trace (partial merge-with union) {:enabled (last enabled)})))
 
