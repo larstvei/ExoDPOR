1021582d50220f1038387e2bf9c80e76f66c1042
Lars Tveito <larstvei@ifi.uio.no>
2020-02-04 10:28:28 +0100

    Add sleep sets

--- src/exogenous/core.clj
+++ src/exogenous/core.clj
@@ -3,15 +3,13 @@
 
 (def search-state (atom {}))
 
-(defn submit [trace enabled]
-  (swap! search-state dpor/add-trace trace enabled))
+(defn submit [trace enabled interference]
+  (swap! search-state dpor/add-trace trace enabled interference))
 
 (defn explore [sim]
   (apply submit (sim []))
   (loop [backtrack (dpor/backtrack @search-state)]
     (when-not (empty? backtrack)
       (doseq [t backtrack]
-        (let [[trace enabled] (sim t)]
-          (prn trace)
-          (submit trace enabled)))
+        (apply submit (sim t)))
       (recur (dpor/backtrack @search-state)))))
--- src/exogenous/dpor.clj
+++ src/exogenous/dpor.clj
@@ -1,14 +1,38 @@
 (ns exogenous.dpor
   (:require [clojure.set :refer [union difference]]))
 
-(defn add-trace [search-state trace enabled]
+(defn new-node [ev enabled]
+  {:backset #{ev} :enabled enabled :blocked #{} :sleep #{ev}})
+
+(defn update-node [node ev enabled]
+  (merge-with union node (new-node ev enabled)))
+
+(defn next-sleep-set [node ev sleep interference]
+  (or node
+      (let [independent? (comp not interference (partial vector ev))
+            next-sleep (filter independent? sleep)]
+        {:sleep (into #{} next-sleep)})))
+
+(defn add-trace [search-state trace enabled interference]
   (-> (fn [s i]
-        (let [state {:backset #{} :enabled (enabled i) :blocked #{} :sleep #{(trace i)}}]
-          (update s (subvec trace 0 i) (partial merge-with union) state)))
+        (let [t1 (subvec trace 0 i)
+              t2 (subvec trace 0 (inc i))
+              ev (trace i)
+              node (update-node (s t1) ev (enabled i))
+              next-node (next-sleep-set (s t2) ev (:sleep node) interference)]
+          (-> (assoc s t1 node)
+              (assoc t2 next-node))))
       (reduce search-state (range (count trace)))
       (update trace (partial merge-with union) {:enabled (last enabled)})))
 
 (defn backtrack [search-state]
+  (-> (fn [[trace {:keys [:backset :sleep]}]]
+        (map (partial conj trace) (difference backset sleep)))
+      (mapcat search-state)))
+
+(defn backtrack-naive [search-state]
   (-> (fn [[trace {:keys [:enabled :sleep]}]]
         (map (partial conj trace) (difference enabled sleep)))
       (mapcat search-state)))
+
+#_(def backtrack backtrack-naive)
