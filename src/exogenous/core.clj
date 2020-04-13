4857919f977545a4ae9e19775ea43e0acac3b377
Lars Tveito <larstvei@ifi.uio.no>
2020-04-13 16:23:08 +0200

    Let explore take an optional options map

--- src/exogenous/core.clj
+++ src/exogenous/core.clj
@@ -7,16 +7,19 @@
 (defn submit [prefix trace rels]
   (swap! search-state dpor/add-trace prefix trace rels))
 
-(defn explore [sim]
-  (reset! search-state {})
-  (loop [backtrack [[]] executed {}]
-    (if (empty? backtrack)
-      [(count executed) (reduce + (map count (vals executed)))]
-      (let [prefix (first backtrack)
-            {:keys [trace mhb interference]} (sim prefix)
-            rels (rel/make-rels trace mhb interference)]
-        (submit prefix trace rels)
-        (recur (dpor/backtrack {:strategy :depth-first
-                                :search-state @search-state
-                                :trace trace})
-               (update executed (:hb rels) conj trace))))))
+(defn explore
+  ([sim] (explore sim {}))
+  ([sim options]
+   (reset! search-state {})
+   (loop [backtrack [[]] executed {}]
+     (if (empty? backtrack)
+       [(count executed) (reduce + (map count (vals executed)))]
+       (let [prefix (first backtrack)
+             {:keys [trace mhb interference]} (sim prefix)
+             rels (rel/make-rels trace mhb interference)]
+         (submit prefix trace rels)
+         (recur (dpor/backtrack (merge {:strategy :depth-first
+                                        :search-state @search-state
+                                        :trace trace}
+                                       options))
+                (update executed (:hb rels) conj trace)))))))
