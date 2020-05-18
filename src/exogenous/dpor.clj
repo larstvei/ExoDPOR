larstvei/ExoDPOR

commit 47da47e76c31e4f26834ccb8f0622b95f203eaca

A more sensible implementation of revisiting nodes and sleep sets

src/exogenous/dpor.clj
@@ -5,21 +5,23 @@
 (defn new-node [ev enabled]
   {:backset #{ev} :enabled enabled :blocked #{} :sleep #{}})
 
-(defn update-node [node ev enabled]
-  (if (empty? (:backset node))
-    (merge-with union node (new-node ev enabled))
-    node))
-
-(defn next-sleep-set [node ev sleep {:keys [hb]}]
-  (or node
-      (->> (remove #(relates? hb ev %) sleep)
-           (into #{})
-           (assoc node :sleep))))
-
 (defn enabled-after [trace i {:keys [mhb]}]
   (let [pre (set (subvec trace 0 i))
         post (subvec trace i)]
     (enabled-candidates post pre mhb)))
+
+(defn update-node [ss trace i {:keys [hb] :as rels}]
+  (let [pre (subvec trace 0 i)
+        ev (trace i)
+        node (ss pre)]
+    (if-not node
+      (new-node ev (enabled-after trace i rels))
+      (let [new-backset (conj (:backset node) ev)
+            prev-sleep (if (empty? pre) #{} (:sleep (ss (pop pre))))
+            new-sleep (set (remove #(relates? hb ev %) prev-sleep))]
+        (-> node
+            (assoc :backset new-backset)
+            (update :sleep union new-sleep))))))
 
 (defn not-dep [trace i {hb :hb}]
   (let [ev (trace i)
@@ -59,19 +61,15 @@
 (defn update-backsets [search-state trace i rels]
   (let [t (subvec trace 0 i)]
     (->> (filter (fn [j] (reversible-race? trace i j rels)) (range i))
-         (reduce (fn [s j] (update-backset s trace i j rels)) search-state))))
+         (reduce (fn [ss j] (update-backset ss trace i j rels)) search-state))))
 
 (defn add-trace [search-state seed-trace trace rels]
-  (-> (fn [s i]
-        (let [t1 (subvec trace 0 i)
-              t2 (subvec trace 0 (inc i))
+  (-> (fn [ss i]
+        (let [pre (subvec trace 0 i)
               ev (trace i)
-              enabled (enabled-after trace i rels)
-              node (update-node (s t1) ev enabled)
-              next-node (next-sleep-set (s t2) ev (:sleep node) rels)]
-          (-> (assoc s t1 node)
-              (update-in [t1 :sleep] conj ev)
-              (assoc t2 next-node)
+              node (update-node ss trace i rels)]
+          (-> (assoc ss pre node)
+              (update-in [pre :sleep] conj ev)
               (update-backsets trace i rels))))
       (reduce search-state (range (count trace)))
       (assoc-in [trace :enabled] (enabled-after trace (count trace) rels))))

commit 542b81649ba0776b6c41542024455d3c49b17853

Fix another bug introduced in 47da47e
The root got special treatment, causing its sleep-set to be reset.

src/exogenous/dpor.clj
@@ -15,8 +15,8 @@
         ev (trace i)
         node (ss pre)
         enabled (enabled-after trace i rels)]
-    (cond (zero? i) (new-node ev enabled #{})
-          node (update node :backset conj ev)
+    (cond node (update node :backset conj ev)
+          (zero? i) (new-node ev enabled #{})
           :else (let [prev (subvec trace 0 (dec i))
                       prev-ev (last prev)
                       prev-sleep (disj (:sleep (ss prev)) prev-ev)